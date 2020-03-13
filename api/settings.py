8de85c663a2c37bced4536ee111565a444461897
Ryan Bishop <rdbishop06@gmail.com>
2020-03-13 10:36:20 -0500

    setup: browsable API

--- api/settings.py
+++ api/settings.py
@@ -77,6 +77,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+    'http://127.0.0.1:3000'
+)
+
 ROOT_URLCONF = 'api.urls'
 
 TEMPLATES = [
