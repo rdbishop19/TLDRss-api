a487ae3acaa6adb7b38853821ed2b3da89165971
Ryan Bishop <rdbishop06@gmail.com>
2020-03-13 20:02:42 -0500

    test: article get working

--- tldrss/tests/test_articles.py
+++ tldrss/tests/test_articles.py
@@ -1,7 +1,9 @@
 import json
+from unittest.mock import patch
 from django.test import TestCase, Client
 from tldrss.models import Article, Feed
 from rest_framework.reverse import reverse
+from datetime import datetime
 
 TEST_CONTENT = [
     {
@@ -24,6 +26,8 @@ class ArticleEndpointTestCase(TestCase):
     '''RSS Articles'''
     maxDiff = None
 
+    # https://docs.python.org/3/library/unittest.mock.html
+    @patch('django.utils.timezone.now', lambda: '2020-03-06T05:34:55.663133Z')
     def test_get(self):
         '''Test we can get a list of articles'''
         client = Client()
@@ -38,14 +42,22 @@ class ArticleEndpointTestCase(TestCase):
             description='Up-to-date news on coronavirus',
             link='https://www.who.int/',
             feed_id=feed.id,
-            created_at='2020-03-06T05:34:55.663133Z',
+            # created_at='2020-03-06T05:34:55.663133Z',  # don't need this with @patch
             pub_date='2020-03-13T05:34:55.663133Z'
         )
+        # list view
+        response = client.get('/article')
+        # make sure the list view returns a 1-length array
+        self.assertEqual(len(response.data['results']), 1)
 
+        # detail view
         response = client.get('/article/1')
-        response.data.pop('created_at', None)
-        # print(response)
-        # print(response.content, TEST_CONTENT[0])
+        
+        # valid endpoint response
         self.assertEqual(response.status_code, 200)
-        print(response.content, TEST_CONTENT[0])
+        # make sure the content matching how we need it to look above
         self.assertEqual(json.loads(response.content), TEST_CONTENT[0])
+
+        # invalid entry
+        bad_response = client.get('article/2')
+        self.assertEqual(bad_response.status_code, 404)
