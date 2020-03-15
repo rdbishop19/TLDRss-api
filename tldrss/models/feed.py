58a40f2b0d7b74998c4345f8eb803da558758e10
Ryan Bishop <rdbishop06@gmail.com>
2020-03-15 15:50:47 -0500

    test: get/post new feed sources

--- tldrss/models/feed.py
+++ tldrss/models/feed.py
@@ -4,4 +4,7 @@ class Feed(models.Model):
     '''Feed model'''
 
     name = models.CharField(max_length=255)
-    feed_url = models.URLField(max_length=255)
+    feed_url = models.URLField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ('name',)
