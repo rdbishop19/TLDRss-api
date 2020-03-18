aa2e53698a00f6ded39a8dfd6dfb15f99cd99401
Ryan Bishop <rdbishop06@gmail.com>
2020-03-18 13:42:49 -0500

    refactor: article model with null pub date

--- tldrss/models/article.py
+++ tldrss/models/article.py
@@ -8,5 +8,5 @@ class Article(models.Model):
     description = models.TextField()
     link = models.URLField(unique=True)
     created_at = models.DateTimeField(auto_now_add=True, auto_now=False)
-    pub_date = models.DateTimeField(auto_now_add=False, auto_now=False)
+    pub_date = models.DateTimeField(auto_now_add=False, auto_now=False, null=True)
     feed = models.ForeignKey(Feed, on_delete=models.CASCADE)
