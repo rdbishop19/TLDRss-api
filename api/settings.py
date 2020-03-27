5314873b5fc174e077f8cd4ce67bc92e2d88e452
Ryan Bishop <rdbishop06@gmail.com>
2020-03-26 23:39:55 -0500

    initial setup for postgresql

--- api/settings.py
+++ api/settings.py
@@ -111,13 +111,23 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': os.path.join(BASE_DIR, 'tldrss'),
+        'NAME': 'tldrss',
+        'USER': 'tldrssuser',
+        'PASSWORD': '3jnVm@zN19ID',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
--- tldrss/models/article.py
+++ tldrss/models/article.py
@@ -7,7 +7,7 @@ class Article(models.Model):
 
     title = models.CharField(max_length=255)
     description = models.TextField()
-    link = models.URLField(unique=True)
+    link = models.URLField(max_length=1024, unique=True)
     created_at = models.DateTimeField(auto_now_add=True, auto_now=False)
     pub_date = models.DateTimeField(auto_now_add=False, auto_now=False, null=True)
     feed = models.ForeignKey(Feed, on_delete=models.CASCADE)
